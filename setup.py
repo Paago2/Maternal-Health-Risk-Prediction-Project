e163ad22558d7d75791a42f545fb8521f26bea3c
Paago2 <paago2@yahoo.com>
2025-04-13 20:34:36 -0400

    project template created, Python environment setup and packages installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="marternal_health",
+    version="0.0.0",
+    author="Paago",
+    author_email="kpoobaripaago@gmail.com",
+    packages=find_packages()
+)
